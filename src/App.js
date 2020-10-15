8d158e70f6745400c2002f76a6fcd0fa45aceb10
ignatif <ignatif@gmail.com>
2020-10-15 14:52:07 +0300

    tiptap in react

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default () => {
+  const [value, setValue] = useState('👋 Hi from React')
+
+  useEffect(() => {
+    let editor = document.createElement('tiptap-react')
+    editor.value = value
+    editor.setValue = setValue
+
+    let script = document.createElement('script')
+    script.src = 'https://tiptap-basic.vercel.app/js/app.57725fc1.js' // vue js bundle
+
+    let css = document.createElement('link')
+    css.rel = 'stylesheet'
+    css.href = 'https://tiptap-basic.vercel.app/css/app.da25fe12.css' // vue css bundle
+
+    document.getElementById('editor-wrapper').appendChild(editor)
+    document.body.appendChild(script)
+    document.head.appendChild(css)
+  }, [])
+
+  useEffect(() => {
+    console.log('value:', value)
+  }, [value])
+
+  return <div id="editor-wrapper" />
 }
-
-export default App;
